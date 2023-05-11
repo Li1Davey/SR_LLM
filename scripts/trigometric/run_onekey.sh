22aa0ba6a68b7058db414e80cfdddf3d700e00dd
Nan Jiang <jiang631@bell-fe03.rcac.purdue.edu>
2023-05-11 16:50:17 -0400

    add running scripts  Changes to be committed: 	new file:   run_onekey.sh 	new file:   submit_jobs_gp_egp_comparison.bell.sh

---
+++ scripts/trigometric/run_onekey.sh
@@ -0,0 +1,7 @@
+./submit_jobs_gp_egp_comparison.bell.sh inv 2 11
+./submit_jobs_gp_egp_comparison.bell.sh inv 3 22
+./submit_jobs_gp_egp_comparison.bell.sh inv 4 46
+./submit_jobs_gp_egp_comparison.bell.sh inv 5 55
+./submit_jobs_gp_egp_comparison.bell.sh inv 5 58
+./submit_jobs_gp_egp_comparison.bell.sh inv 6 68
+./submit_jobs_gp_egp_comparison.bell.sh inv 6 610
