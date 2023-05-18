98ab6ba90727f312d31c09bb943567f0fc321246
Nan Jiang <jiang631@purdue.edu>
2023-05-18 15:34:39 -0400

    add dso

---
+++ dso_classic/dso/dso/baselines/eureqa/scripts/onekey.sh
@@ -0,0 +1,22 @@
+#./large_data.dso.desktop.55.sh 2 11 inv
+#./large_data.dso.desktop.55.sh 3 22 inv
+./large_data.dso.desktop.55.sh 4 46 inv
+#./large_data.dso.desktop.55.sh 5 55 inv
+#./large_data.dso.desktop.55.sh 5 58 inv
+#./large_data.dso.desktop.55.sh 6 68 inv
+#./large_data.dso.desktop.55.sh 6 610 inv
+#
+#./large_data.dso.desktop.55.sh 2 11 sincos 
+#./large_data.dso.desktop.55.sh 3 22 sincos
+#./large_data.dso.desktop.55.sh 5 55 sincos
+#./large_data.dso.desktop.55.sh 5 58 sincos
+#./large_data.dso.desktop.55.sh 6 68 sincos
+#./large_data.dso.desktop.55.sh 6 610 sincos
+
+#./large_data.dso.desktop.55.sh 2 11 sincosinv
+#./large_data.dso.desktop.55.sh 3 22 sincosinv
+#./large_data.dso.desktop.55.sh 4 46 sincosinv
+#./large_data.dso.desktop.55.sh 5 55 sincosinv
+#./large_data.dso.desktop.55.sh 5 58 sincosinv
+#./large_data.dso.desktop.55.sh 6 68 sincosinv
+#./large_data.dso.desktop.55.sh 6 610 sincosinv
