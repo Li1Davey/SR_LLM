7e9376eaa22f319f62ba56f374c45c17a30e0682
Nan Jiang <jiang631@purdue.edu>
2023-05-31 00:53:54 -0400

    update

---
+++ ctrl_var_gp_nan/scripts/others/submit_jobs_gp_egp.bell.sh
@@ -0,0 +1,65 @@
+#!/bin/bash -l
+
+basepath=/depot/yexiang/apps/jiang631/data/scibench
+
+thispath=$basepath/ctrl_var_gp_nan
+data_path=$basepath/data/unencrypted/equations_others
+py3615=/home/jiang631/workspace/miniconda3/envs/py3615/bin/python3
+
+noise_type=normal
+noise_scale=0.0
+metric_name=neg_mse
+all_equations=$(ls $data_path/*.in)
+for eq_name in $all_equations; do
+	echo "Submitted $eq_name"
+	short_name=$(basename "$eq_name")
+	trimed_name=${short_name:0:-3}
+	dump_dir=$basepath/result/Others/$(date +%F)
+	if [ ! -d "$dump_dir" ]; then
+		echo "create dir: $dump_dir"
+		mkdir -p $dump_dir
+	fi
+	log_dir=$basepath/log/Ohters/$(date +%F)/
+	if [ ! -d "$log_dir" ]; then
+		echo "create dir: $log_dir"
+		mkdir -p $log_dir
+	fi
+	sbatch -A yexiang --nodes=1 --ntasks=1 --cpus-per-task=1 <<EOT
+#!/bin/bash -l
+
+#SBATCH --job-name="gp_$trimed_name"
+#SBATCH --output=$log_dir/${short_name}.metric_${metric_name}.noise_${noise_type}_${noise_scale}.gp.out
+#SBATCH --constraint=A
+#SBATCH --time=48:00:00
+#SBATCH --mem=2048MB
+
+hostname
+
+
+
+$py3615 $thispath/try_gp_xyx.py --equation_name $eq_name \
+        		--metric_name 'neg_mse' --noise_type $noise_type --noise_scale $noise_scale \
+        		 > $dump_dir/${short_name}.metric_${metric_name}.noise_${noise_type}_${noise_scale}.gp.out
+
+EOT
+
+#	sbatch -A yexiang --nodes=1 --ntasks=1 --cpus-per-task=1 <<EOT
+##!/bin/bash -l
+#
+##SBATCH --job-name="egp_$trimed_name"
+##SBATCH --output=$log_dir/${short_name}.metric_${metric_name}.noise_${noise_type}_${noise_scale}.egp.out
+##SBATCH --constraint=A
+##SBATCH --time=48:00:00
+##SBATCH --mem=2048MB
+#
+#hostname
+#
+#
+#
+#$py3615 $thispath/try_gp_xyx.py --equation_name $eq_name --expand_gp \
+#        		--metric_name 'neg_mse' --noise_type $noise_type --noise_scale $noise_scale \
+#        		 > $dump_dir/${short_name}.metric_${metric_name}.noise_${noise_type}_${noise_scale}.egp.out
+#
+#EOT
+
+done
