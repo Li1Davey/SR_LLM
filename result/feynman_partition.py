ae52a099ede3b3fd9559d74775f7d58f3d1272fb
Nan Jiang <jiang631@purdue.edu>
2023-06-06 06:41:02 -0400

    partition the feynman dataset

--- result/feynman_partition.py
+++ result/feynman_partition.py
@@ -1,25 +1,26 @@
-medium = set(
-    ['FeynmanICh8Eq14', 'FeynmanICh10Eq7', 'FeynmanICh11Eq19', 'FeynmanICh12Eq2', 'FeynmanICh12Eq11', 'FeynmanICh13Eq4', 'FeynmanICh13Eq12',
-     'FeynmanICh15Eq10', 'FeynmanICh16Eq6', 'FeynmanICh18Eq4', 'FeynmanICh24Eq6', 'FeynmanICh29Eq4', 'FeynmanICh32Eq5', 'FeynmanICh34Eq8',
-     'FeynmanICh34Eq10', 'FeynmanICh34Eq27', 'FeynmanICh38Eq12', 'FeynmanICh39Eq10', 'FeynmanICh39Eq11', 'FeynmanICh43Eq31',
-     'FeynmanICh43Eq43', 'FeynmanICh48Eq2', 'FeynmanIICh6Eq11', 'FeynmanIICh8Eq7', 'FeynmanIICh11Eq3', 'FeynmanIICh21Eq32',
-     'FeynmanIICh34Eq2', 'FeynmanIICh34Eq2a', 'FeynmanIICh34Eq29a', 'FeynmanIICh37Eq1', 'FeynmanIIICh4Eq32', 'FeynmanIIICh8Eq54',
-     'FeynmanIIICh13Eq18', 'FeynmanIIICh14Eq14', 'FeynmanIIICh15Eq12', 'FeynmanIIICh15Eq14', 'FeynmanIIICh17Eq37', 'FeynmanIIICh19Eq51',
-     'FeynmanBonus8', 'FeynmanBonus18'])
-easy = set(
-    ['FeynmanICh12Eq1', 'FeynmanICh12Eq4', 'FeynmanICh12Eq5', 'FeynmanICh14Eq3', 'FeynmanICh14Eq4', 'FeynmanICh18Eq12', 'FeynmanICh18Eq16',
-     'FeynmanICh25Eq13', 'FeynmanICh26Eq2', 'FeynmanICh27Eq6', 'FeynmanICh30Eq5', 'FeynmanICh43Eq16', 'FeynmanICh47Eq23',
-     'FeynmanIICh2Eq42', 'FeynmanIICh3Eq24', 'FeynmanIICh4Eq23', 'FeynmanIICh8Eq31', 'FeynmanIICh10Eq9', 'FeynmanIICh13Eq17',
-     'FeynmanIICh15Eq4', 'FeynmanIICh15Eq5', 'FeynmanIICh27Eq16', 'FeynmanIICh27Eq18', 'FeynmanIICh34Eq11', 'FeynmanIICh34Eq29b',
-     'FeynmanIICh38Eq3', 'FeynmanIICh38Eq14', 'FeynmanIIICh7Eq38', 'FeynmanIIICh12Eq43', 'FeynmanIIICh15Eq27'])
-hard = set(['FeynmanICh6Eq20', 'FeynmanICh6Eq20a', 'FeynmanICh6Eq20b', 'FeynmanICh9Eq18', 'FeynmanICh15Eq3t', 'FeynmanICh15Eq3x',
-            'FeynmanICh29Eq16', 'FeynmanICh30Eq3', 'FeynmanICh32Eq17', 'FeynmanICh34Eq14', 'FeynmanICh37Eq4', 'FeynmanICh39Eq22',
-            'FeynmanICh40Eq1', 'FeynmanICh41Eq16', 'FeynmanICh44Eq4', 'FeynmanICh50Eq26', 'FeynmanIICh6Eq15a', 'FeynmanIICh6Eq15b',
-            'FeynmanIICh11Eq17', 'FeynmanIICh11Eq20', 'FeynmanIICh11Eq27', 'FeynmanIICh11Eq28', 'FeynmanIICh13Eq23', 'FeynmanIICh13Eq34',
-            'FeynmanIICh24Eq17', 'FeynmanIICh35Eq18', 'FeynmanIICh35Eq21', 'FeynmanIICh36Eq38', 'FeynmanIIICh4Eq33', 'FeynmanIIICh9Eq52',
-            'FeynmanIIICh10Eq19', 'FeynmanIIICh21Eq20', 'FeynmanBonus1', 'FeynmanBonus2', 'FeynmanBonus3', 'FeynmanBonus4', 'FeynmanBonus5',
-            'FeynmanBonus6', 'FeynmanBonus7', 'FeynmanBonus9', 'FeynmanBonus10', 'FeynmanBonus11', 'FeynmanBonus12', 'FeynmanBonus13',
-            'FeynmanBonus14', 'FeynmanBonus15', 'FeynmanBonus16', 'FeynmanBonus17', 'FeynmanBonus19', 'FeynmanBonus20'])
+var1 = ['FeynmanICh6Eq20a', 'FeynmanICh29Eq4', 'FeynmanICh34Eq27', 'FeynmanIICh8Eq31', 'FeynmanIICh27Eq16', 'FeynmanIICh27Eq18',
+        'FeynmanIIICh12Eq43', ]
+var2 = ['FeynmanICh12Eq1', 'FeynmanICh6Eq20', 'FeynmanICh10Eq7', 'FeynmanICh12Eq4', 'FeynmanICh14Eq3', 'FeynmanICh12Eq5', 'FeynmanICh14Eq4',
+        'FeynmanICh15Eq10', 'FeynmanICh16Eq6', 'FeynmanICh25Eq13', 'FeynmanICh26Eq2', 'FeynmanICh32Eq5', 'FeynmanICh34Eq10',
+        'FeynmanICh34Eq14', 'FeynmanICh38Eq12', 'FeynmanICh39Eq10', 'FeynmanICh41Eq16', 'FeynmanICh43Eq31', 'FeynmanICh48Eq2',
+        'FeynmanIICh3Eq24', 'FeynmanIICh4Eq23', 'FeynmanIICh8Eq7', 'FeynmanIICh10Eq9', 'FeynmanIICh11Eq28', 'FeynmanIICh13Eq17',
+        'FeynmanIICh13Eq23', 'FeynmanIICh13Eq34', 'FeynmanIICh24Eq17', 'FeynmanIICh34Eq29a', 'FeynmanIICh38Eq14', 'FeynmanIIICh4Eq32',
+        'FeynmanIIICh4Eq33', 'FeynmanIIICh7Eq38', 'FeynmanIIICh8Eq54', 'FeynmanIIICh15Eq14', 'FeynmanBonus8', 'FeynmanBonus10', ]
+var3 = ['FeynmanICh6Eq20b', 'FeynmanICh12Eq2', 'FeynmanICh15Eq3t', 'FeynmanICh15Eq3x', 'FeynmanBonus20', 'FeynmanICh18Eq12',
+        'FeynmanICh27Eq6', 'FeynmanICh30Eq3', 'FeynmanICh30Eq5', 'FeynmanICh37Eq4', 'FeynmanICh39Eq11', 'FeynmanICh39Eq22',
+        'FeynmanICh43Eq43', 'FeynmanICh47Eq23', 'FeynmanIICh6Eq11', 'FeynmanIICh6Eq15b', 'FeynmanIICh11Eq27', 'FeynmanIICh15Eq4',
+        'FeynmanIICh15Eq5', 'FeynmanIICh21Eq32', 'FeynmanIICh34Eq2a', 'FeynmanIICh34Eq2', 'FeynmanIICh34Eq29b', 'FeynmanIICh37Eq1',
+        'FeynmanIIICh13Eq18', 'FeynmanIIICh15Eq12', 'FeynmanIIICh15Eq27', 'FeynmanIIICh17Eq37', 'FeynmanIIICh19Eq51', 'FeynmanBonus5',
+        'FeynmanBonus7', 'FeynmanBonus9', 'FeynmanBonus15', 'FeynmanBonus18', ]
+var4 = ['FeynmanICh8Eq14', 'FeynmanICh13Eq4', 'FeynmanICh13Eq12', 'FeynmanICh18Eq4', 'FeynmanICh18Eq16', 'FeynmanICh24Eq6',
+        'FeynmanICh29Eq16', 'FeynmanICh32Eq17', 'FeynmanICh34Eq8', 'FeynmanICh40Eq1', 'FeynmanICh43Eq16', 'FeynmanICh44Eq4',
+        'FeynmanICh50Eq26', 'FeynmanIICh11Eq20', 'FeynmanIICh34Eq11', 'FeynmanIICh35Eq18', 'FeynmanIICh35Eq21', 'FeynmanIICh38Eq3',
+        'FeynmanIIICh10Eq19', 'FeynmanIIICh14Eq14', 'FeynmanIIICh21Eq20', 'FeynmanBonus1', 'FeynmanBonus3', 'FeynmanBonus11',
+        'FeynmanBonus19', ]
+var5 = ['FeynmanICh12Eq11', 'FeynmanIICh2Eq42', 'FeynmanIICh6Eq15a', 'FeynmanIICh11Eq3', 'FeynmanIICh11Eq17', 'FeynmanIICh36Eq38',
+        'FeynmanIIICh9Eq52', 'FeynmanBonus4', 'FeynmanBonus12', 'FeynmanBonus13', 'FeynmanBonus14', 'FeynmanBonus16', ]
+var678 = ['FeynmanICh11Eq19', 'FeynmanBonus2', 'FeynmanBonus17', 'FeynmanBonus6', 'FeynmanICh9Eq18', ]
+
 output = """neg_nmse	DSR	PQT	VPG	GPMELD
 FeynmanICh43Eq31	-0.652023319	-0.730470266	-0.7332186147	-0.6973430462
 FeynmanIIICh19Eq51	-1.007309833	-1.006795248	-1.003691455	-0.9136057663
@@ -948,7 +949,7 @@ FeynmanICh12Eq4	1.39E-23	1.40E-23	3.32E-23	2.07E-23
 FeynmanICh47Eq23	0.9999999974	0.9999999981	0.9999999982	0.9999999982
 FeynmanIICh11Eq27	1	1	1	1"""
 
-output="""FeynmanICh43Eq31	-1.258473296	-1.275934484
+output = """FeynmanICh43Eq31	-1.258473296	-1.275934484
 FeynmanICh29Eq16	-0.1260685211	-0.21176973
 FeynmanIICh24Eq17	nan	nan
 FeynmanICh16Eq6	-0.0002370418857	nan
@@ -1071,7 +1072,7 @@ print("EASY")
 for line in output.split("\n"):
     spl = line.split("\t")
     if len(line) <= 1:
-        print(line+'\n')
+        print(line + '\n')
     elif spl[0] in ['neg_nmse', 'neg_nrmse', 'inv_nrmse', 'inv_nmse', 'neg_mse', 'neg_rmse', 'neglog_mse', 'inv_mse']:
         print(line)
     elif spl[0] in easy:
@@ -1081,7 +1082,7 @@ print("\nMEDIUM")
 for line in output.split("\n"):
     spl = line.split("\t")
     if len(line) <= 1:
-        print(line+'\n')
+        print(line + '\n')
     elif spl[0] in ['neg_nmse', 'neg_nrmse', 'inv_nrmse', 'inv_nmse', 'neg_mse', 'neg_rmse', 'neglog_mse', 'inv_mse']:
         print(line)
     elif spl[0] in medium:
@@ -1091,7 +1092,7 @@ print("\nHARD")
 for line in output.split("\n"):
     spl = line.split("\t")
     if len(line) <= 1:
-        print(line+'\n')
+        print(line + '\n')
     elif spl[0] in ['neg_nmse', 'neg_nrmse', 'inv_nrmse', 'inv_nmse', 'neg_mse', 'neg_rmse', 'neglog_mse', 'inv_mse']:
         print(line)
     elif spl[0] in hard:
