477c0b4dce6ccc222c424d363136fb722d3fa386
Nan Jiang <jiang631@purdue.edu>
2023-06-30 19:49:10 -0400

    Changes to be committed: 	modified:   trigometric/submit_jobs_gp_egp_comparison.bell.sh

--- tree_cvgp_nan/scripts/trigometric/submit_jobs_gp_egp_comparison.bell.sh
+++ tree_cvgp_nan/scripts/trigometric/submit_jobs_gp_egp_comparison.bell.sh
@@ -39,7 +39,7 @@ do
 
 hostname
 
-$py3615 $thispath/main.py --equation_name $data_path/$eq_name --expand_gp \
+$py3615 $thispath/main.py --equation_name $data_path/$eq_name \
         		--metric_name 'neg_mse' --noise_type $noise_type --noise_scale $noise_scale \
         		 > $dump_dir/prog_${prog}.metric_${metric_name}.noise_${noise_type}${noise_scale}.treegp.out
 
