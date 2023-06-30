5c1337023462c56b9c3960d05333292ac7a71b76
Nan Jiang <jiang631@purdue.edu>
2023-06-30 19:47:56 -0400

    Changes to be committed: 	modified:   trigometric/submit_jobs_gp_egp_comparison.bell.sh

--- tree_cvgp_nan/scripts/trigometric/submit_jobs_gp_egp_comparison.bell.sh
+++ tree_cvgp_nan/scripts/trigometric/submit_jobs_gp_egp_comparison.bell.sh
@@ -7,7 +7,7 @@ nt=$3
 
 thispath=$basepath/tree_cvgp_nan
 data_path=$basepath/data/unencrypted/equations_trigometric
-
+py3615=/home/jiang631/workspace/miniconda3/envs/py3615/bin/python3
 
 noise_type=normal
 noise_scale=0.0
@@ -39,10 +39,7 @@ do
 
 hostname
 
-module load anaconda
-
-
-python3 $thispath/main.py --equation_name $data_path/$eq_name --expand_gp \
+$py3615 $thispath/main.py --equation_name $data_path/$eq_name --expand_gp \
         		--metric_name 'neg_mse' --noise_type $noise_type --noise_scale $noise_scale \
         		 > $dump_dir/prog_${prog}.metric_${metric_name}.noise_${noise_type}${noise_scale}.treegp.out
 
