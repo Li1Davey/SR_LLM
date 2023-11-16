42752319da96567686dd249f32b8c567671b285a
Nan Jiang <jiang631@purdue.edu>
2023-11-16 17:02:46 -0500

    add noisy  Changes to be committed: 	modified:   scripts/trigometric/run_cv_mcts_bell_noisy.sh 	modified:   scripts/trigometric/run_mcts_bell_noisy.sh

--- mcts_and_cvmcts/scripts/trigometric/run_mcts_bell_noisy.sh
+++ mcts_and_cvmcts/scripts/trigometric/run_mcts_bell_noisy.sh
@@ -31,7 +31,7 @@ for noise_scale in 0.1 0.02 0.04 0.08 0.12 0.14; do
 		sbatch -A yexiang --nodes=1 --ntasks=1 --cpus-per-task=1 <<EOT
 #!/bin/bash -l
 
-#SBATCH --job-name="mcts-${type}${nv}${nt}"
+#SBATCH --job-name="TS-No${type}${nv}${nt}"
 #SBATCH --output=$log_dir/${eq_name}.metric_${metric_name}.noise_${noise_type}_${noise_scale}.opt${opt}.mcts.out
 #SBATCH --constraint=A
 #SBATCH --time=12:00:00
