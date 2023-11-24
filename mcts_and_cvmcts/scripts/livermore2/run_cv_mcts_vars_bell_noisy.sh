ddf029a110cc13af45a49e0baf07317bccdf4c22
Nan Jiang <jiang631@purdue.edu>
2023-11-24 01:37:41 -0500

    run livermore2  Changes to be committed: 	modified:   production_rules.py 	new file:   scripts/livermore2/run_cv_mcts_vars_bell.sh 	new file:   scripts/livermore2/run_cv_mcts_vars_bell_noisy.sh 	new file:   scripts/livermore2/run_mcts_vars_bell.sh 	new file:   scripts/livermore2/run_mcts_vars_bell_noisy.sh 	renamed:    ../src/scibench/scibench/encrypted_equations/equation_pde.py -> ../src/scibench/scibench/encrypted_equations/equation_pde_materials.py

---
+++ mcts_and_cvmcts/scripts/livermore2/run_cv_mcts_vars_bell_noisy.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -l
+
+basepath=/depot/yexiang/apps/jiang631/data/scibench
+type=Livermore2
+nv=$1
+
+thispath=$basepath/mcts_and_cvmcts
+data_path=$basepath/data/unencrypted/equations_others
+py310=/home/jiang631/workspace/miniconda3/envs/py310/bin/python3
+opt=L-BFGS-B
+
+noise_type=normal
+noise_scale=0.1
+metric_name=neg_mse
+for prog in {1..25};
+do
+    eq_name=${type}_Vars${nv}_$prog.in
+    echo "submit $eq_name"
+        dump_dir=$basepath/result/${type}_Vars${nv}/$(date +%F)
+    if [ ! -d "$dump_dir" ]
+    then
+        echo "create dir: $dump_dir"
+        mkdir -p $dump_dir
+        fi
+        log_dir=$basepath/log/$(date +%F)
+        if [ ! -d "$log_dir" ]
+        then
+        echo "create dir: $log_dir"
+        mkdir -p $log_dir
+        fi
+    sbatch -A yexiang --nodes=1 --ntasks=1 --cpus-per-task=1 <<EOT
+#!/bin/bash -l
+
+#SBATCH --job-name="CvMtNoVr$eq_name"
+#SBATCH --output=$log_dir/${eq_name}.metric_${metric_name}.noise_${noise_type}_${noise_scale}.cv_mcts.out
+#SBATCH --constraint=A
+#SBATCH --time=12:00:00
+#SBATCH --mem=10240MB
+
+hostname
+
+$py310 $thispath/main.py --equation_name $data_path/$eq_name --optimizer $opt \
+						--metric_name $metric_name --noise_type $noise_type --noise_scale $noise_scale \
+         				> $dump_dir/${eq_name}.metric_${metric_name}.noise_${noise_type}${noise_scale}.cv_mcts.out
+
+EOT
+
+done
