004ac303d5af1abaf11cc0008e42f462a23838c5
Nan Jiang <jiang631@purdue.edu>
2023-12-04 01:17:05 -0500

    debug

--- gp_and_cvgp/scripts/trigometric/run_recovery_rate_comparison.bell.sh
+++ gp_and_cvgp/scripts/trigometric/run_recovery_rate_comparison.bell.sh
@@ -29,14 +29,15 @@ for prog in {0..9}; do
 #!/bin/bash -l
 
 #SBATCH --job-name="GP${type}${nv}${nt}_${prog}"
-#SBATCH --output=$log_dir/${eq_name}.metric_${metric_name}.noise_${noise_type}_${noise_scale}.gp.out
+#SBATCH --output=$log_dir/${eq_name}.metric_${metric_name}.noise_${noise_type}_${noise_scale}.opt$opt.gp.out
 #SBATCH --constraint=A
 #SBATCH --time=12:00:00
 #SBATCH --mem=4GB
 
 hostname
 
-$py310 $thispath/try_gp_xyx.py --equation_name $data_path/$eq_name  --optimizer $opt			--track_memory \
+$py310 $thispath/main.py --equation_name $data_path/$eq_name  --optimizer $opt \
+				--track_memory \
 				--memray_output_bin $dump_dir/prog_${prog}.metric_${metric_name}.noise_${noise_type}${noise_scale}.opt$opt.gp.bin \
         		--metric_name $metric_name --noise_type $noise_type --noise_scale $noise_scale \
         		 > $dump_dir/prog_${prog}.metric_${metric_name}.noise_${noise_type}_${noise_scale}.opt$opt.gp.out
@@ -54,7 +55,7 @@ EOT
 
 hostname
 
-$py310 $thispath/try_gp_xyx.py --equation_name $data_path/$eq_name --cvgp --optimizer $opt		--track_memory \
+$py310 $thispath/main.py --equation_name $data_path/$eq_name --cvgp --optimizer $opt		--track_memory \
 		--memray_output_bin $dump_dir/prog_${prog}.metric_${metric_name}.noise_${noise_type}${noise_scale}.opt$opt.cvgp.bin \
         --metric_name $metric_name --noise_type $noise_type --noise_scale $noise_scale \
         		 > $dump_dir/prog_${prog}.metric_${metric_name}.noise_${noise_type}${noise_scale}.opt$opt.cvgp.out
