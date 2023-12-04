c99e38d1f42396c1a411ca147714325af3961e62
Nan Jiang <jiang631@purdue.edu>
2023-12-03 20:59:37 -0500

    unify runing scripts

--- mcts_and_cvmcts/scripts/trigometric/run_mcts_bell.sh
+++ mcts_and_cvmcts/scripts/trigometric/run_mcts_bell.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/zsh
-set -x
 
 
 basepath=/depot/yexiang/apps/jiang631/data/scibench
@@ -15,21 +14,18 @@ opt=L-BFGS-B
 noise_type=normal
 noise_scale=0.0
 metric_name=neg_mse
-for prog in {0..9};
-do
-    eq_name=${type}_nv${nv}_nt${nt}_prog_${prog}.in
-    echo "submit $eq_name"
-   	dump_dir=$basepath/result/${type}_nv${nv}_nt${nt}/$(date +%F)
-    if [ ! -d "$dump_dir" ]
-    then
-    	echo "create dir: $dump_dir"
-    	mkdir -p $dump_dir
+for prog in {0..9}; do
+	eq_name=${type}_nv${nv}_nt${nt}_prog_${prog}.in
+	echo "submit $eq_name"
+	dump_dir=$basepath/result/${type}_nv${nv}_nt${nt}/$(date +%F)
+	if [ ! -d "$dump_dir" ]; then
+		echo "create dir: $dump_dir"
+		mkdir -p $dump_dir
 	fi
 	log_dir=$basepath/log/$(date +%F)
-	if [ ! -d "$log_dir" ]
-	then
-    	echo "create dir: $log_dir"
-    	mkdir -p $log_dir
+	if [ ! -d "$log_dir" ]; then
+		echo "create dir: $log_dir"
+		mkdir -p $log_dir
 	fi
 	echo "$dump_dir/prog_${prog}.metric_${metric_name}.noise_${noise_type}${noise_scale}.opt$opt.out"
 	sbatch -A yexiang --nodes=1 --ntasks=1 --cpus-per-task=1 <<EOT
@@ -42,9 +38,6 @@ do
 #SBATCH --mem=10240MB
 
 hostname
-$py310 $thispath/main.py --equation_name $data_path/$eq_name --optimizer $opt\
-        		--metric_name 'neg_mse' --noise_type $noise_type --noise_scale $noise_scale > $dump_dir/prog_${prog}.metric_${metric_name}.noise_${noise_type}${noise_scale}.opt$opt.mcts.out
+$py310 $thispath/main.py --equation_name $data_path/$eq_name --optimizer $opt        		--metric_name $metric_name --noise_type $noise_type --noise_scale $noise_scale > $dump_dir/prog_${prog}.metric_${metric_name}.noise_${noise_type}${noise_scale}.opt$opt.mcts.out
 EOT
 done
-
-
